36f416bb3ce4cfbdeafbdd03e12204f0a4bdc60d
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-12-06 10:08:32 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  state = {
+    exercises: [],
+    title: ''
+  }
+
+
+  render() {
+    return <h1>Exercises</h1>
+  }
+}
