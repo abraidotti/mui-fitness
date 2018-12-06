24b8b4f54177cb177cdf1b95fcbef9c3afca5c64
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-12-06 10:26:28 -0500

    add partial form

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import {
+  Button,
+  Paper,
+  TextField,
+  Typography
+} from '@material-ui/core/Paper'
 
 export default class App extends Component {
   state = {
@@ -6,8 +12,34 @@ export default class App extends Component {
     title: ''
   }
 
+  handleChange = ({ target: { name, value } }) =>
+    this.setState({
+      [name]: value
+    })
 
   render() {
-    return <h1>Exercises</h1>
+    const { title } = this.state
+    return <Paper>
+      <Typography variant="display1" align="center" gutterBottom>
+        Exercises
+      </Typography>
+
+      <form>
+        <TextField
+          name="title"
+          label="Exercise"
+          value={title}
+          onChange={this.handleChange}
+          margin="normal"
+        />
+        <Button
+          type="submit"
+          color="primary"
+          variant="raised"
+        >
+          Create
+        </Button>
+      </form>
+    </Paper>
   }
 }
