9d21a990664dfc4575b701ec4398705233f02d7e
Alessandro Braidotti <alessandro.braidotti@gmail.com>
2018-12-06 11:17:56 -0500

    add working form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react'
 import {
   Button,
+  IconButton,
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
   Paper,
   TextField,
   Typography
-} from '@material-ui/core/Paper'
+} from '@material-ui/core'
+import { Delete } from '@material-ui/icons'
 
 export default class App extends Component {
   state = {
-    exercises: [],
+    exercises: [
+      { id: 1, title: 'Bro sweats' },
+      { id: 2, title: 'Cheese rinds' },
+      { id: 3, title: 'Juice smacks' }
+    ],
     title: ''
   }
 
@@ -17,29 +27,69 @@ export default class App extends Component {
       [name]: value
     })
 
-  render() {
-    const { title } = this.state
-    return <Paper>
-      <Typography variant="display1" align="center" gutterBottom>
-        Exercises
-      </Typography>
+  handleCreate = event => {
+    event.preventDefault()
 
-      <form>
-        <TextField
-          name="title"
-          label="Exercise"
-          value={title}
-          onChange={this.handleChange}
-          margin="normal"
-        />
-        <Button
-          type="submit"
-          color="primary"
-          variant="raised"
-        >
-          Create
-        </Button>
-      </form>
-    </Paper>
+    if (this.state.title) {
+      this.setState(({ exercises, title }) => ({
+        exercises: [
+          ...exercises,
+          {
+            title,
+            id: Date.now()
+          }
+        ],
+        title: ''
+      }))
+    }
+  }
+
+  handleDelete = id =>
+    this.setState(({ exercises }) => ({
+      exercises: exercises.filter(exercise => exercise.id !== id)
+    }))
+
+  render() {
+    const { title, exercises } = this.state
+    return (
+      <Paper>
+        <Typography variant="display1" align="center" gutterBottom>
+          Exercises
+        </Typography>
+
+        <form onSubmit={this.handleCreate}>
+          <TextField
+            name="title"
+            label="Exercise"
+            value={title}
+            onChange={this.handleChange}
+            margin="normal"
+          />
+          <Button
+            type="submit"
+            color="primary"
+            variant="raised"
+          >
+            Create
+          </Button>
+        </form>
+
+        <List>
+          {exercises.map(({ id, title }) =>
+            <ListItem key={id}>
+              <ListItemText primary={title} />
+              <ListItemSecondaryAction>
+                <IconButton
+                  color="primary"
+                  onClick={() => this.handleDelete(id)}
+                >
+                  <Delete />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          )}
+        </List>
+      </Paper>
+    )
   }
 }
